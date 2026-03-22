69115c053b7dbacb6d44054721e2b853094f872b
SamarthSable <samarthsable02@gmail.com>
2026-03-22 11:33:14 +0530

    changes in product color component

--- e-commerce-app/src/components/product/ProductColor.jsx
+++ e-commerce-app/src/components/product/ProductColor.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function ProductColor({
   colorList,
-  onColorSelect,
+  onColorSelect = () => {},
   readOnly = false,
 }) {
   const [activeColor, setActiveColor] = useState("");
